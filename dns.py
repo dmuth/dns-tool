8069bae3c73be6170ca375d5a29b890a5cdf10c9
Douglas Muth <dmuth@dmuth.org>
2018-01-11 21:32:50 -0500

    Put header parsing code into parseHeader().

--- dns.py
+++ dns.py
@@ -27,14 +27,56 @@ logger.info("Args: %s" % args)
 logger.setLevel(logging.DEBUG)
 
 
+def parseHeader(data):
+	"""
+	parseHeader(): Extracts the various fields of our header
+	
+	Returns a dictionary.
+	"""
+
+	retval = {}
+
+	retval["request_id"] = binascii.hexlify(data[0:2])
+
+	#
+	# Header flag bits:
+	#
+	# 0 - QR: 0 if query, 1 if answer
+	# 1-4 - Opcode: 0 is standard query, 1 is reverse query, 2 is server status request
+	# 5 - AA: Is the answer authoritative?
+	# 6 - TC: Has the message been truncated?
+	# 7 - RD: Set to 1 when recursion is desired
+	# 8 - RA: Is Recursion available on this DNS server?
+	# 9-11 - Z: These reserved bits are always set to zero.
+	# 12-15 - RCODE: Result Code.  0 for no errors.
+	#
+	logger.debug("Header Flags: %s: %s %s" % (binascii.hexlify(data[2:4]), bin(ord(data[2])), bin(ord(data[3]))))
+
+	retval["header"] = {}
+	retval["header"]["qr"] = (ord(data[2]) & 0b10000000) >> 7
+	retval["header"]["aa"] = (ord(data[2]) & 0b01111000) >> 3
+	retval["header"]["rd"] = (ord(data[2]) & 0b00000001)
+	retval["header"]["ra"] = (ord(data[3]) & 0b10000000) >> 7
+	retval["header"]["z"]  = (ord(data[3]) & 0b01110000) >> 4
+	retval["header"]["rcode"] = (ord(data[3]) & 0b00001111)
+	
+	
+	retval["num_questions"] = binascii.hexlify(data[4:6])
+	retval["num_answers"] = binascii.hexlify(data[6:8])
+	retval["num_authority_records"] = binascii.hexlify(data[8:10])
+	retval["num_additional_records"] = binascii.hexlify(data[10:12])
+
+	return(retval)
+
+
 #
 # TODO: 
+# Deconstruct Question section: put into a function for reusability with query
+# Deconstruct answer section: also put into a function
 # sendUdpMessage(): Return a data structure with some parsng
 # Argument for query to pass in
 # Look up code as per http://www.tcpipguide.com/free/t_DNSMessageHeaderandQuestionSectionFormat.htm
-# Deconstruct Question section: put into a function for reusability with query
 #
-
 def sendUdpMessage(message, address, port):
     """sendUdpMessage sends a message to UDP server
 
@@ -48,42 +90,9 @@ def sendUdpMessage(message, address, port):
         sock.sendto(binascii.unhexlify(message), server_address)
         data, _ = sock.recvfrom(4096)
 
-	logger.info("answer(): Request ID: %s" % binascii.hexlify(data[0:2]))
-	logger.info("answer(): Flags: %s" % binascii.hexlify(data[2:4]))
-	#
-	# Header flag bits:
-	#
-	# 0 - QR: 0 if query, 1 if answer
-	# 1-4 - Opcode: 0 is standard query, 1 is reverse query, 2 is server status request
-	# 5 - AA: Is the answer authoritative?
-	# 6 - TC: Has the message been truncated?
-	# 7 - RD: Set to 1 when recursion is desired
-	# 8 - RA: Is Recursion available on this DNS server?
-	# 9-11 - Z: These reserved bits are always set to zero.
-	# 12-15 - RCODE: Result Code.  0 for no errors.
-	#
-	logger.debug("Header flag bits: %s %s" % (bin(ord(data[2])), bin(ord(data[3]))))
+	header = parseHeader(data)
+	logger.info("Header: %s" % header)
 
-	qr = (ord(data[2]) & 0b10000000) >> 7
-	aa = (ord(data[2]) & 0b01111000) >> 3
-	rd = (ord(data[2]) & 0b00000001)
-	ra = (ord(data[3]) & 0b10000000) >> 7
-	z  = (ord(data[3]) & 0b01110000) >> 4
-	rcode = (ord(data[3]) & 0b00001111)
-	
-	logger.info("Header: QR: %s AA: %s RD: %s RA: %s Z: %s RCODE: %s" % (
-		binascii.hexlify(chr(qr)), 
-		binascii.hexlify(chr(aa)), 
-		binascii.hexlify(chr(rd)), 
-		binascii.hexlify(chr(ra)), 
-		binascii.hexlify(chr(z)), 
-		binascii.hexlify(chr(rcode))
-		))
-	
-	logger.info("answer(): Number of questions: %s" % binascii.hexlify(data[4:6]))
-	logger.info("answer(): Number of answers: %s" % binascii.hexlify(data[6:8]))
-	logger.info("answer(): Number of authority records: %s" % binascii.hexlify(data[8:10]))
-	logger.info("answer(): Number of additional records: %s" % binascii.hexlify(data[10:12]))
 
     finally:
         sock.close()
