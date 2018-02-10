39ef5abd2f42f82eea5ac8c28fea80e554b9cae6
Douglas Muth <dmuth@dmuth.org>
2018-02-10 18:28:36 -0500

    Now printing the actual text of the question when graphing

--- output.py
+++ output.py
@@ -72,7 +72,7 @@ def printQuestion(args, question, response):
 		print("                                1  1  1  1  1  1")
 		print("     0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5")
 		print("   +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+")
-		print("   |  QNAME:                                       |")
+		print("   |  QNAME: %-30s        |" % question["question"])
 		for row in question["meta"]["data_decoded"]:
 			length = row["length"]
 			string = "(nil)"
