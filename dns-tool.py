1e84ca4ba0eb4fa5f9ba36d63efa48dcdbc0d5a0
Douglas Muth <dmuth@dmuth.org>
2018-02-11 18:45:32 -0500

    Split up sendUdpMessage() into 3 different functions.

--- dns-tool.py
+++ dns-tool.py
@@ -55,62 +55,55 @@ elif args.quiet:
 
 logger.info("Args: %s" % args)
 
-def sendUdpMessage(message, address, port):
-	"""sendUdpMessage(message, address, port): sends a message to UDP server
 
-	message can be a raw string
+def getDnsMessage(args):
 	"""
-    
-	retval = {}
+	getDnsMessage(args): Construct our DNS message to send
+	"""
 
-	server_address = (address, port)
+	header = create.createHeader()
+	logger.debug(parse.parseHeader(header))
+
+	question = create.createQuestion(args.query, args.query_type)
+	logger.debug(parse_question.parseQuestion(0, question))
+
+	retval = header + question
+
+	return(retval)
+
+
+def sendDnsMessage(args, message):
+	"""
+	sendDnsMessage(args, message): Send our DNS message (unless we're reading from stdin) and then return the result.
+	"""
+
+	retval = ""
+
+	#
+	# If we're reading from standard input, do that right here.
+	#
+	if args.stdin:
+		# Source: https://stackoverflow.com/a/38939320/196073
+		#source = sys.stdin.buffer # Python 3
+		source = sys.stdin
+		retval = source.read()
+		return(retval)
+
+	#
+	# Otherwise, looks like we're sending an actual DNS query!
+	#
+
+	server_address = (args.server, 53)
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	#
-	# Set our timeout
-	#
 	sock.settimeout(3)
 
 	try:
-		logger.info("Sending query to %s:%s..." % (address, port))
+		logger.info("Sending query to %s:%s..." % (args.server, 53))
 		sock.sendto(message, server_address)
 		#sock.sendto(bytearray(message, "iso8859-1"), server_address)
-		data, _ = sock.recvfrom(4096)
-
-		if args.stdin:
-			# Source: https://stackoverflow.com/a/38939320/196073
-			#source = sys.stdin.buffer # Python 3
-			source = sys.stdin
-			data = source.read()
-
-		if args.raw:
-			# Source: https://stackoverflow.com/a/4849792/196073
-			#sys.stdout.buffer.write(data) # Python 3
-
-			# https://stackoverflow.com/a/2374443/196073
-			for c in data:
-				sys.stdout.write(c)
-			sys.exit(0)
-
-		request_id = parse.getRequestId(message)
-
-		retval["server"] = server_address
-		retval["header"] = parse.parseHeader(data[0:12])
-		retval["question"] = parse_question.parseQuestion(12, data)
-
-		#
-		# Send us past the headers and question and parse the answer(s).
-		#
-		answer_index = 12 + retval["question"]["question_length"]
-		retval["answers"] = parse_answer.parseAnswers(data, question_length = retval["question"]["question_length"])
-
-		#
-		# Do a sanity check on the results.
-		#
-		retval["sanity"] = sanity.go(retval["header"], retval["answers"], request_id)
-
-		retval["raw"] = output.formatHex(data)
+		retval, _ = sock.recvfrom(4096)
 
 	except socket.error as e:
 		logger.error("Error connecting to %s:%s: %s" % (address, port, e))
@@ -122,16 +115,62 @@ def sendUdpMessage(message, address, port):
 	return(retval)
 
 
-header = create.createHeader()
-logger.debug(parse.parseHeader(header))
+def parseMessage(args, message):
+	"""
+	parseMessage(args, message): Parse our message and return a data structure of that.
+	"""
 
-question = create.createQuestion(args.query, args.query_type)
-logger.debug(parse_question.parseQuestion(0, question))
+	retval = {}
 
-message = header + question
+	if args.raw:
+		# Source: https://stackoverflow.com/a/4849792/196073
+		#sys.stdout.buffer.write(data) # Python 3
 
-response = sendUdpMessage(message, args.server, 53)
+		# https://stackoverflow.com/a/2374443/196073
+		for c in message:
+			sys.stdout.write(c)
+		sys.exit(0)
 
+	request_id = parse.getRequestId(message)
+
+	retval["server"] = args.server
+	retval["header"] = parse.parseHeader(message[0:12])
+	retval["question"] = parse_question.parseQuestion(12, message)
+
+	#
+	# Send us past the headers and question and parse the answer(s).
+	#
+	answer_index = 12 + retval["question"]["question_length"]
+	retval["answers"] = parse_answer.parseAnswers(message, question_length = retval["question"]["question_length"])
+
+	#
+	# Do a sanity check on the results.
+	#
+	retval["sanity"] = sanity.go(retval["header"], retval["answers"], request_id)
+
+	retval["raw"] = output.formatHex(message)
+
+	return(retval)
+
+
+#
+# Get our DNS message (be it assembling the message or reading from stdin)
+#
+message = getDnsMessage(args)
+
+#
+# Send out the DNS message (be it a query or writing to stdout and then exiting)
+#
+message = sendDnsMessage(args, message)
+
+#
+# Parse our message that we got from the DNS server or stdin.
+#
+response = parseMessage(args, message)
+
+#
+# Print out the parsed response
+#
 output.printResponse(args, response)
 
 
