a04c2150dec9acd44131865c4a3b16e9f4032bbe
Douglas Muth <dmuth@dmuth.org>
2018-07-26 21:32:18 -0400

    Removed blurb about /mnt.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,12 +6,6 @@
 # Errors are fatal
 set -e
 
-echo "# "
-echo "# The dns-tool app has been installed and can be run by typing 'dns-tool'."
-echo "# "
-echo "# If you want to do any development, just cd into /mnt/ and you can run dns-tool from there."
-echo "# "
-
 #
 # Run whatever was passed in
 #
